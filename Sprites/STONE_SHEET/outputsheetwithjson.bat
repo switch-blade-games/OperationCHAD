d5e8d3491f377184708752204a86265080eccb2a
Juan Campana <switchbladegames@protonmail.com>
2019-12-17 16:50:32 -0700

    worked on sprites

--- Sprites/STONE_SHEET/outputsheetwithjson.bat
+++ Sprites/STONE_SHEET/outputsheetwithjson.bat
@@ -1,5 +1,5 @@
 @echo off
-set aseprite="F:\Gamedev\GameMaker\Tools\Aseprite\Aseprite.exe"
-set project="F:\Gamedev\GameMaker\Tools\Aseprite\projects\stone.aseprite"
+set aseprite="C:\Users\Johnny B\Downloads\Aseprite.v1.2.9.x64.APP-DARKSiDERS\Aseprite.exe"
+set project="C:\Users\Johnny B\Desktop\Operation CHAD\100-Character Sprites\100-Stone_test.aseprite"
 
-%aseprite% %project% -b --sheet sheet.png --sheet-type packed --data sheetinfo.json --list-tags
+%aseprite% %project% -b --sheet STONE_TEST.png --sheet-type rows --sheet-width 1000 --data sheetinfo.json --list-tags
